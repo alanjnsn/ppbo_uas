04c2b6209f892e5a19de16059fe4e9ebd3b32b82
Alan Jansen Situmorang <143371272+alanjnsn@users.noreply.github.com>
2024-06-16 15:42:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,55 +8,41 @@ import time
 
 
 class Database:
+    sudah_ujian=[]
+    data=[('alan','123'),('nazril','456')]
+
     def login(self):
-        laman_login = tk.Tk()
-        laman_login.title("Laman Login")
-        laman_login.attributes('-fullscreen',True)
-        laman_login.configure(background='white')
+        self.laman_login = tk.Tk()
+        self.laman_login.title("Laman Login")
+        self.laman_login.attributes('-fullscreen',True)
+        self.laman_login.configure(background='white')
 
-        data=[('alan','123'),('nazril','456')]
-        sudah_ujian=[]
-        NIK = tk.StringVar()
-        PASSWORD = tk.StringVar()
-
-        def tombol_click():
-            user_nik = nik_entry.get().lower()
-            user_pass = password_entry.get().lower()
-            if (user_nik,user_pass) in data:
-                    konfirmasi=messagebox.askquestion("Success", "mulai ujian")
-                    if konfirmasi=='yes':
-                        sudah_ujian.append(user_nik)
-                        laman_login.destroy()
-                    else:
-                        pass
-            else:
-                messagebox.showerror("Input Error", "username atau password salah, coba masukkan yang benar")
-                nik_entry.delete(0, tk.END)
-                password_entry.delete(0, tk.END)
+        self.NIK = tk.StringVar()
+        self.PASSWORD = tk.StringVar()
                
-        input_frame = ttk.Frame(laman_login)
-        input_frame.pack(padx=10,pady=10,fill="x")
+        self.input_frame = ttk.Frame(self.laman_login)
+        self.input_frame.pack(padx=10,pady=10,fill="x")
 
-        keterangan_frame = ttk.Frame(laman_login)
-        keterangan_frame.pack(side='bottom',padx=10,pady=10,fill="x")
+        self.keterangan_frame = ttk.Frame(self.laman_login)
+        self.keterangan_frame.pack(side='bottom',padx=10,pady=10,fill="x")
 
-        judul = ttk.Label(input_frame,text="SELAMAT DATANG PESERTA UTBK",font=("Helvetica", 16))
-        judul.pack(anchor='center', pady=20)
+        self.judul = ttk.Label(self.input_frame,text="SELAMAT DATANG PESERTA UTBK",font=("Helvetica", 16))
+        self.judul.pack(anchor='center', pady=20)
 
-        nik_label = ttk.Label(input_frame,text="NIK:",font=("Helvetica", 12))
-        nik_label.pack(padx=10,expand=True)
-        nik_entry = ttk.Entry(input_frame,textvariable=NIK,font=("Helvetica", 12))
-        nik_entry.pack(padx=10,expand=True)
+        self.nik_label = ttk.Label(self.input_frame,text="NIK:",font=("Helvetica", 12))
+        self.nik_label.pack(padx=10,expand=True)
+        self.nik_entry = ttk.Entry(self.input_frame,textvariable=self.NIK,font=("Helvetica", 12))
+        self.nik_entry.pack(padx=10,expand=True)
 
-        password_label = ttk.Label(input_frame,text="Password:",font=("Helvetica", 12))
-        password_label.pack(padx=10,expand=True)
-        password_entry = ttk.Entry(input_frame,textvariable=PASSWORD,font=("Helvetica", 12))
-        password_entry.pack(padx=10,expand=True)
+        self.password_label = ttk.Label(self.input_frame,text="Password:",font=("Helvetica", 12))
+        self.password_label.pack(padx=10,expand=True)
+        self.password_entry = ttk.Entry(self.input_frame,textvariable=self.PASSWORD,font=("Helvetica", 12))
+        self.password_entry.pack(padx=10,expand=True)
 
-        tombol_login = ttk.Button(input_frame,text="Login!",command=tombol_click)
-        tombol_login.pack(side='bottom',padx=10,pady=10)
+        self.tombol_login = ttk.Button(self.input_frame,text="Login!",command=self.tombol_click)
+        self.tombol_login.pack(side='bottom',padx=10,pady=10)
 
-        teks = ttk.Label(keterangan_frame,text=f'''\tImplementasi sederhana Gambaran\n
+        teks = ttk.Label(self.keterangan_frame,text=f'''\tImplementasi sederhana Gambaran\n
             Aplikasi Ujian SNBT berbasis GUI dan MVC\n\n        
             \t\tKelompok 5:\n        
             \t\t    Alan\n        
@@ -65,8 +51,108 @@ class Database:
             \t\t    Marsa''',font=("Helvetica", 14))
         teks.pack(anchor='center',pady=20)
 
-        laman_login.mainloop()
+        self.laman_login.mainloop()
+
+    def tombol_click(self):
+            user_nik = self.nik_entry.get().lower()
+            user_pass = self.password_entry.get().lower()
+            if (user_nik,user_pass) in self.data:
+                if user_nik in self.__class__.sudah_ujian:
+                    messagebox.showinfo("Input Error", "Sesi anda sudah habis")
+                    self.nik_entry.delete(0, tk.END)
+                    self.password_entry.delete(0, tk.END)
+                else:
+                    konfirmasi=messagebox.askquestion("Success", "mulai ujian")
+                    if konfirmasi=='yes':
+                        self.sudah_ujian.append(user_nik)
+                        self.laman_login.quit()
+                        self.laman_login.destroy()
+                    else:
+                        pass
+            else:
+                messagebox.showerror("Input Error", "username atau password salah, coba masukkan yang benar")
+                self.nik_entry.delete(0, tk.END)
+                self.password_entry.delete(0, tk.END)
+
+    def nilai_ujian(self,ujian):
+        self.ujian= ujian          
+        self.hasil = tk.Tk()
+        self.hasil.title("Diagram Jawaban")
+        self.hasil.configure(bg='white')
+        self.hasil.attributes("-fullscreen", True)
         
+        def chart(axes, sizes, labels, colors, title):
+            axes.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+            axes.set_title(title)
+
+        for i in self.ujian.jawaban_mtk:
+            try:
+                self.ujian.total += int(i)
+            except:
+                pass
+
+        for i in self.ujian.jawaban_bi:
+            try:
+                self.ujian.total += float(i)
+            except:
+                pass
+        self.ujian.total/=2
+
+        self.persebaran_mtk = [0, 0, 0]
+
+        for i in self.ujian.jawaban_mtk:
+            if i == '20':
+                self.persebaran_mtk[0] += 1
+            elif i == 'none':
+                self.persebaran_mtk[2] += 1
+            else:
+                self.persebaran_mtk[1] += 1
+
+        self.persebaran_bi = [0, 0, 0]
+
+        for i in self.ujian.jawaban_bi:
+            if i == '12.5':
+                self.persebaran_bi[0] += 1
+            elif i == 'none':
+                self.persebaran_bi[2] += 1
+            else:
+                self.persebaran_bi[1] += 1
+
+        fig,(ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+        kategori = ['Benar', 'Salah', 'Tidak Dijawab']
+        warna = ['green', 'red', 'gray']
+        chart(ax1, self.persebaran_mtk, kategori, warna, 'Persebaran Jawaban matematika')
+        chart(ax2, self.persebaran_bi, kategori, warna, 'Persebaran Jawaban bahasa indonesia')
+
+        pemberitahuan = ''
+        latar_belakang = ''
+        if self.ujian.total >= 70:
+            pemberitahuan = 'SELAMAT ANDA BERHASIL DALAM TES INI'
+            latar_belakang = 'blue'
+        else:
+            pemberitahuan = 'MAAF ANDA BELUM BERHASIL DALAM TES INI'
+            latar_belakang = 'red'
+
+        label1 = tk.Label(self.hasil, text=pemberitahuan, font=("Arial", 27))
+        label1.configure(bg=latar_belakang)
+        label1.pack(padx=20, pady=5)
+        label2 = tk.Label(self.hasil, text=f"Nilai anda: {self.ujian.total}%\nKKM: 70%", font=("Arial", 24))
+        label2.pack(padx=20, pady=5)
+
+        self.canvas = FigureCanvasTkAgg(fig, master=self.hasil)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(anchor='center')
+        self.frame_penutup = tk.Frame(self.hasil)
+        self.frame_penutup.pack(side="bottom", fill="x")
+
+        self.penutup = tk.Label(self.frame_penutup, text=f'info selanjutnya akan diumumkan kemendikbud.id', font=("Arial", 20), anchor='s')
+        self.penutup.pack(padx=20, pady=5)
+
+        self.finish_button = tk.Button(self.frame_penutup, text="OK", command=lambda:[self.hasil.quit(),self.hasil.destroy()], font=("Arial", 10))
+        self.finish_button.pack(side="right", padx=25, pady=10)
+        self.hasil.update_idletasks()
+        self.hasil.mainloop()
 
 
 class ExamModel:
@@ -308,7 +394,6 @@ class ExamView:
             self.next_button.pack(side="right", padx=25, pady=10)
             self.previous_button.pack(side="left", padx=25, pady=10)
 
-
     def b_indo(self,user):
         self.sisa_waktu = 60
         self.user=user
@@ -336,6 +421,7 @@ class ExamView:
         self.timer2=self.user.laman_bi.after(1000, self.update_timer2)
         self.user.window.mainloop()
         self.jawaban_bi = [var.get() for var in self.user.jawaban_bi]
+        self.user.window.quit()
 
     def update_timer2(self):
         if self.sisa_waktu > 0:
@@ -345,6 +431,7 @@ class ExamView:
             self.user.laman_bi.after(1000, self.update_timer2)
         else:
             self.user.laman_bi.pack_forget()
+            self.user.window.quit()
             self.user.window.destroy()
 
     def kalkulasi2(self):
@@ -373,94 +460,6 @@ class ExamView:
             self.previous_button.pack(side="left", padx=25, pady=10)
 
 
-class Diagram():
-    def __init__(self,hasil):
-        self.hasil= hasil          
-        def chart(axes, sizes, labels, colors, title):
-            axes.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-            axes.set_title(title)
-
-        for i in self.hasil.jawaban_mtk:
-            try:
-                self.hasil.total += int(i)
-            except:
-                pass
-
-        for i in self.hasil.jawaban_bi:
-            try:
-                self.hasil.total += float(i)
-            except:
-                pass
-        self.hasil.total/=2
-        persebaran_mtk = [0, 0, 0]
-
-        for i in self.hasil.jawaban_mtk:
-            if i == '20':
-                persebaran_mtk[0] += 1
-            elif i == 'none':
-                persebaran_mtk[2] += 1
-            else:
-                persebaran_mtk[1] += 1
-
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-        
-        for i in self.hasil.jawaban_bi:
-            try:
-                self.hasil.total += int(i)
-            except:
-                pass
-
-        persebaran_bi = [0, 0, 0]
-
-        for i in self.hasil.jawaban_bi:
-            if i == '12.5':
-                persebaran_bi[0] += 1
-            elif i == 'none':
-                persebaran_bi[2] += 1
-            else:
-                persebaran_bi[1] += 1
-
-        kategori = ['Benar', 'Salah', 'Tidak Dijawab']
-        warna = ['green', 'red', 'gray']
-        chart(ax1, persebaran_mtk, kategori, warna, 'Persebaran Jawaban matematika')
-        chart(ax2, persebaran_bi, kategori, warna, 'Persebaran Jawaban bahasa indonesia')
-
-        hasil = tk.Tk()
-        hasil.title("Diagram Jawaban")
-        hasil.configure(bg='white')
-        hasil.attributes("-fullscreen", True)
-
-        pemberitahuan = ''
-        latar_belakang = ''
-        if self.hasil.total >= 70:
-            pemberitahuan = 'SELAMAT ANDA BERHASIL DALAM TES INI'
-            latar_belakang = 'blue'
-        else:
-            pemberitahuan = 'MAAF ANDA BELUM BERHASIL DALAM TES INI'
-            latar_belakang = 'red'
-
-        label1 = tk.Label(hasil, text=pemberitahuan, font=("Arial", 27))
-        label1.configure(bg=latar_belakang)
-        label1.pack(padx=20, pady=5)
-        label2 = tk.Label(hasil, text=f"Nilai anda: {self.hasil.total}%\nKKM: 70%", font=("Arial", 24))
-        label2.pack(padx=20, pady=5)
-
-        canvas = FigureCanvasTkAgg(fig, master=hasil)
-        canvas.draw()
-        canvas.get_tk_widget().pack(anchor='center')
-
-        frame_penutup = tk.Frame(hasil)
-        frame_penutup.pack(side="bottom", fill="x")
-
-        penutup = tk.Label(frame_penutup, text=f'info selanjutnya akan diumumkan kemendikbud.id', font=("Arial", 20), anchor='s')
-        penutup.pack(padx=20, pady=5)
-
-        finish_button = tk.Button(frame_penutup, text="OK", command=hasil.destroy, font=("Arial", 10))
-        finish_button.pack(side="right", padx=25, pady=10)
-
-        hasil.mainloop()
-
-
 class ExamController:
     def __init__(self, view, model):
         self.model = model
@@ -468,9 +467,19 @@ class ExamController:
         self.view.mulai_ujian(self.model)
 
 
-login= Database()
-login.login()
+
+database= Database()
+
+# sesi 1
+database.login()
 model1 = ExamModel()
 view1 = ExamView()
 controller1 = ExamController(view1, model1)
-diagram= Diagram(view1)
+database.nilai_ujian(controller1.view)
+
+# sesi 2
+database.login()
+model2 = ExamModel()
+view2 = ExamView()
+controller2 = ExamController(view2, model2)
+database.nilai_ujian(view2)
